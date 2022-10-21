a7b2cf4782b35a5fa8bc61b0f6d235b9f9d7c018
Kanishka Samarathunga <kanishkakcs@gmail.com>
2022-10-22 02:15:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ const usersRoutes = require('./routes/users-routes')
 
 const app = express()
 
-app.use(cors())
 app.use(bodyParser.json())
 
 app.use('/uploads/images', express.static(path.join('uploads', 'images')))
